d2d46cbd89edc415bb2eb55bc70c5be93e5096a0
Clement Delafargue <clement@delafargue.name>
2016-07-12 00:06:01 +0200

    Save positions sent by the Spot beacon

--- routes/email.js
+++ routes/email.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const busboy = require('connect-busboy');
 const router = express.Router();
+const crypto = require('crypto');
+
+const key = process.env.MAILGUN_API_KEY;
+const checkSignature = (timestamp, token, signature) => {
+  const hmac = crypto.createHmac('sha256', key);
+  hmac.update(timestamp + token);
+  const digest = hmac.digest('hex');
+  return digest === signature;
+}
 
 router.use(busboy({ immediate: true }));
 router.post('/inbound-email', (req, res, next) => {
-  console.log(req.busboy);
-  console.log(req.body);
-  req.busboy.on('field', (k, v, kt, kv) => {
-    console.log({ k, v, kt, kv});
-  })
-  req.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    console.log('got file: ' + fieldname + ' ' + filename);
-  })
-  res.sendStatus(200);
+  if(checkSignature(req.body.timestamp, req.body.token, req.body.signature)) {
+    if(req.body['X-Spot-Type'] === 'NEWMOVEMENT') {
+      GeoData.addPoint({
+        x: req.body['X-Spot-Latitude'],
+        y: req.body['X-Spot-Longitude'],
+        depth: 0,
+        boat: 'sub'
+      }).then(() => res.sendStatus(201));
+    } else {
+      res.sendStatus(200);
+    }
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 module.exports = router;
