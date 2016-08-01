e9ed92015c0f8e435e90efcf31b87296c06fca68
Clement Delafargue <clement@delafargue.name>
2016-08-01 09:35:15 +0200

    Enable compression middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var compression = require('compression');
 
 var routes = require('./routes/index');
 var adminRoutes = require('./routes/admin');
@@ -19,6 +20,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
